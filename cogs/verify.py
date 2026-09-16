7a1d34763212d8c656fb0c663a5f18972275f488
AyoBlue <57001134+AyoBlue@users.noreply.github.com>
2026-09-16 14:12:46 -0400

    Update verify.py

--- cogs/verify.py
+++ cogs/verify.py
@@ -6,36 +6,10 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_interaction(self, interaction: discord.Interaction):
-        if interaction.custom_id == "verify_button":
-            role = discord.utils.get(interaction.guild.roles, id=1295952331933745256)
-            if role:
-                await interaction.user.add_roles(role)
-
-            await interaction.response.defer()
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        guild = self.bot.get_guild(1289017735631867945)
-        if not(guild):
-            return
-        
-        channel = discord.utils.get(guild.channels, id=1549575851073605733)
-        if not(channel):
-            return
-
-        messages = [message async for message in channel.history(limit=1)]
-        if len(messages) > 0:
-            return
-
-        view = discord.ui.View(timeout=None)
-        view.add_item(discord.ui.Button(
-            label = "Verify",
-            custom_id = "verify_button",
-            style = discord.ButtonStyle.green
-        ))
-
-        await channel.send("Welcome to Atlantis!", view=view)
+    async def on_member_join(self, member: discord.Member):
+        role = discord.utils.get(member.guild.roles, id=1295952331933745256)
+        if role:
+            await member.add_roles(role)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Events(bot))
